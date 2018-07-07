86a1b5d6b64059984258a8f256d66ebe2ecf6776
thinkerou <thinkerou@gmail.com>
2018-07-07 17:57:14 +0800

    oneof for protobuf

--- src/main/java/com/thinkerou/springkafkaprotobuf/kafka/Receiver.java
+++ src/main/java/com/thinkerou/springkafkaprotobuf/kafka/Receiver.java
@@ -6,13 +6,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.thinkerou.proto.helloworld.Callback;
+
 @Component
 public class Receiver {
 
     private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
 
     @KafkaListener(topics = {"spring-kafka-protobuf-demo"})
-    public void listen(ConsumerRecord<?, ?> record) {
-        logger.warn("message: {}", record.value());
+    public void listen(ConsumerRecord<?, Callback> record) {
+        Callback callback = record.value();
+        logger.warn("message: {}", callback);
+        switch (callback.getEventTypeCase()) {
+            case ONE_EVENT:
+                logger.warn("one event: {}", callback.getOneEvent());
+                break;
+            case TWO_EVENT:
+                logger.warn("two event: {}", callback.getOneEvent());
+                break;
+            default:
+                logger.warn("default value...");
+                break;
+        }
     }
 }
