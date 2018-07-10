878b75ad547e6077ccec055f4db10efb65c0fedc
田欧 <thinkerou@gmail.com>
2018-07-10 17:11:27 +0800

    Attempt to fix bug using protobuf oneof (#1)
    * oneof for protobuf
    
    * add CallbackSerializer

---
+++ src/main/java/com/thinkerou/springkafkaprotobuf/kafka/CallbackDeserializer.java
@@ -0,0 +1,24 @@
+package com.thinkerou.springkafkaprotobuf.kafka;
+
+import org.apache.kafka.common.serialization.Deserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.thinkerou.proto.helloworld.Callback;
+
+public class CallbackDeserializer  extends Adapter implements Deserializer<Callback> {
+
+    private static final Logger logger = LoggerFactory.getLogger(CallbackDeserializer.class);
+
+    @Override
+    public Callback deserialize(final String topic, byte[] data) {
+        try {
+            return Callback.parseFrom(data);
+        } catch (InvalidProtocolBufferException e) {
+            logger.error("Received un-parse message exception and skip.");
+            return null;
+            // throw new RuntimeException("Received un-parse message " + e.getMessage(), e);
+        }
+    }
+}
